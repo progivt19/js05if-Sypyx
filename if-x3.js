fd002451fa7eb14c276d60ef00a85ec12735414f
Айсен Терентьевич Скрыбыкин <54848331+Sypyx@users.noreply.github.com>
2019-10-22 15:18:44 +0900

    Update if-x3.js

--- if-x3.js
+++ if-x3.js
@@ -2,9 +2,21 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
-
+function digitName(n){
+switch(n){
+    case 0: return'ZERO';
+    case 1: return 'ONE';
+    case 2: return 'TWO';
+    case 3: return 'THREE';
+    case 4: return 'FOUR';
+    case 5: return 'FIVE';
+    case 6: return 'SIX';
+    case 7: return 'SEVEN';
+    case 8: return 'EIGHT';
+    case 9: return 'NINE';
+    default: return 'ERROR'; 
+}
 
 }
 
-module.exports = ...;
+module.exports = digitName;
