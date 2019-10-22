5bcfb5ac2f17639be74024401f67ea9784f5a6c2
Айсен Терентьевич Скрыбыкин <54848331+Sypyx@users.noreply.github.com>
2019-10-22 15:36:09 +0900

    Update if-x4.js

--- if-x4.js
+++ if-x4.js
@@ -2,9 +2,20 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
-
-
+function S(x1, y1, x2, y2,x3, y3){
+    var a=Math.sqrt(Math.sqr(x2-x1)+Math.sqr(y2-y1));
+    var b=Math.sqrt(Math.sqr(x3-x1)+Math.sqr(y3-y1));
+    var c=Math.sqrt(Math.sqr(x3-x2)+Math.sqr(y3-y2));
+    if (a==b)
+    if (Math.sqrt(Math.sqr(a)+Math.sqr(b))==c)
+    return 'true';
+    if (a==c)
+    if (Math.sqrt(Math.sqr(a)+Math.sqr(c))==b)
+    return 'true';
+    if (c==b)
+    if (Math.sqrt(Math.sqr(b)+Math.sqr(c))==a)
+    return 'true';
+    return 'false';
 }
 
-module.exports = ...;
+module.exports = S;
