35d257abc616e3f309e93a85832ad6def41556b7
Айсен Терентьевич Скрыбыкин <54848331+Sypyx@users.noreply.github.com>
2019-10-22 15:09:54 +0900

    Update if-x2.js

--- if-x2.js
+++ if-x2.js
@@ -2,9 +2,12 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
-
-
+function h(a, b, c){
+    if (a+b<c || a+c<c || b+c<a)
+        return 1;
+    var p=(a+b+c)/2;
+    var s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    return s*2/a;
 }
 
-module.exports = ...;
+module.exports = h;
