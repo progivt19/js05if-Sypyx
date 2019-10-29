a5b3083f014b9b392933fe8eeebf763df95c1845
Айсен Терентьевич Скрыбыкин <54848331+Sypyx@users.noreply.github.com>
2019-10-29 09:15:42 +0900

    Update if-00.js

--- if-00.js
+++ if-00.js
@@ -2,9 +2,9 @@
 // f(x,y,z) = min(x+y+z, xyz, xy+z)
 
 function f(x, y, z){
-    if (x+y+z < x*y*z && x+y+z < x*y+z)
+    if (x+y+z <= x*y*z && x+y+z <= x*y+z)
         return x+y+z;
-    if (x*y*z < x+y+z && x*y*z < x*y+z)
+    if (x*y*z <= x+y+z && x*y*z <= x*y+z)
         return x*y*z; 
     return x*y + z;
 }
