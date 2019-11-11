9ff89a86f8ec9be1c53c314130381f9e809f384a
Айсен Терентьевич Скрыбыкин <54848331+Sypyx@users.noreply.github.com>
2019-11-12 08:57:27 +0900

    Update if-x4.js

--- if-x4.js
+++ if-x4.js
@@ -7,12 +7,12 @@ function S(x1, y1, x2, y2,x3, y3){
     var b=Math.sqrt((x3-x1)*(x3-x1)+(y3-y1)*(y3-y1));
     var c=Math.sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2));
     if (a==b && Math.sqrt(a*a+b*b)==c)
-    return 'true';
+    return true;
     if (a==c && Math.sqrt(a*a+c*c)==b)
-    return 'true';
+    return true;
     if (c==b && Math.sqrt(b*b+c*c)==a)
-    return 'true';
-    return 'false';
+    return true;
+    return false;
 }
 
 module.exports = S;
