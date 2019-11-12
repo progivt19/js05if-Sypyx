873e5f26d5fb63aaa7cb5d1b59cd7d34dcaf3585
Айсен Терентьевич Скрыбыкин <54848331+Sypyx@users.noreply.github.com>
2019-11-12 09:35:44 +0900

    Update if-x4_test.js

--- test/if-x4_test.js
+++ test/if-x4_test.js
@@ -7,4 +7,5 @@ QUnit.test("S(x1, y1, x2, y2,x3, y3)=true/false", function(assert){
 	assert.deepEqual(S(2,0,0,2,4,2), true, "S(2,0,0,2,4,2) === true")
 	assert.deepEqual(S(1,1,4,1,1,3), false, "S(1,1,4,1,1,3) === false")
 	assert.deepEqual(S(4,3,9,10,3,0), false, "S(4,3,9,10,3,0) === false")
+	assert.deepEqual(S(-4,3,9,10,3,0), false, "S(-4,3,9,10,3,0) === false")
 });
